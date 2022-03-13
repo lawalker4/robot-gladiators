029a4d9aab7e2193b89568358f9015ded3409a34
LA WALKER <lauraashleywalker4@gmail.com>
2022-03-13 09:47:56 -0400

    update

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,18 +1,6 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-
-// check to see if the value of the playerHealth variable is greater than 0
-if (playerHealth > 0) {
-  console.log("Your player is still alive!");
-}
-var playerHealth = 10;
-
-if (playerHealth === 0) {
-  console.log("This will not run.");
-} 
-else {
-  console.log("This will run instead.");
-}
+var playerAttack= 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -21,11 +9,22 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
-  window.alert("Welcome to Robot Gladiators!");
-};
+// check to see if the value of the playerHealth variable is greater than 0
+// if (playerHealth > 0) {
+//   console.log("Your player is still alive!");
+// }
+// var playerHealth = 10;
 
-fight();
+// if (playerHealth === 0) {
+//   console.log("This will not run.");
+// } 
+// else {
+//   console.log("This will run instead.");
+// }
+
+// var fight = function() {
+//   window.alert("Welcome to Robot Gladiators!");
+// };
 
 var fight = function() {
   // Alert players that they are starting the round
@@ -51,12 +50,13 @@ var fight = function() {
 
     // check enemy's health
     if (enemyHealth <= 0) {
-     window.alert(enemyName + " has died!");
+     window.alert(enemyName + "has died!");
       } 
     else {
     window.alert(enemyName + " still has " + enemyHealth + " health left.");}
   // if player choses to fight, then fight
-if (promptFight === "fight" || promptFight === "FIGHT") {
+
+    if (promptFight === "fight" || promptFight === "FIGHT") {
   // remove enemy's health by subtracting the amount set in the playerAttack variable
   enemyHealth = enemyHealth - playerAttack;
   console.log(
@@ -91,5 +91,5 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   
   }
 
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-
+//  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+fight();
